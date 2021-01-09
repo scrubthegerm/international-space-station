901547f436b92f08b97cfd18b11348b44b62c267
scrubthegerm <76744615+scrubthegerm@users.noreply.github.com>
2021-01-09 15:23:58 -0500

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -2,22 +2,22 @@
 # To make this config usable, you need to rename it to config.py as opposed to exampleconfig.py!
 # To make this config usable, you need to rename it to config.py as opposed to exampleconfig.py!!
 
-token = "Put your token here."
+token = "Nzk3NTU5MTE2MjcxOTc2NDg4.X_oOzw.HioY6RNFLwMccVO5MODhgk4eG14"
 
 dev_ids = [213508710063013888, 213497002548396032]
 # Put the ids of the developers, without quotes, here. Just like that. You can put as many or as few as you want.
 
-prefix = "Put your prefix here."
+prefix = "!"
 
-bot_guild_invite = "Put an invite to the bot's official/support guild here."
+bot_guild_invite = "https://discord.gg/M8aDkmFBMf"
 
-bot_oauth2_invite = "Put the bot's invite link here."
+bot_oauth2_invite = "https://discord.gg/M8aDkmFBMf"
 
 info_channel = 0
 # Used for things like server joins. If you don't want this, just keep this as "0".
 
-nasa_api_key = "Want one? Go here: https://api.nasa.gov/#apply-for-an-api-key"
+nasa_api_key = "QaOn7iAMusKRdbDfJoOd6i8x2JrprQCzDOUhrWDc"
 
-mapbox_api_key = "You need this for iss.now to work."
+mapbox_api_key = "sk.eyJ1IjoiZ2VybXlnZXJtIiwiYSI6ImNranE1a3FmaTNhcjgyc21wMXI3ZGhxbnIifQ.UnxWBNfag0TPmuTzmrU-fw"
 
-owm_api_key = "You need this for iss.weather to work."
+owm_api_key = "69d87279810b896d388996e4ff3fd995"
